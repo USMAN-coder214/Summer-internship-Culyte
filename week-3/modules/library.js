e4e86f84b59abaf76a6eed8cbb4c0f9e7f4ac4f2
M Usman Khan <musmank334@gmail.com>
2026-07-30 22:31:49 -0700

    week3 Day 4 and 5: <what you worked on today>

---
+++ week-3/modules/library.js
@@ -0,0 +1,12 @@
+
+export function add(a,b){
+    return a+b;
+}
+export function sub(a,b){
+    return a-b;
+}
+// Using export default
+
+export default function subs(a,b){
+    return a-b;
+}
---
+++ week-3/modules/main.js
@@ -0,0 +1,10 @@
+import { add ,sub} from "./library.js";
+
+console.log(add(4,4));
+console.log(sub(10,5));
+
+//Using default
+
+import  subs from "./library.js";
+
+console.log(sub(12,6));
